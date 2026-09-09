31cc41fc3020e701c0dd9179671f77005b6fd27f
r-rony08 <robiul.rony1008@gmail.com>
2026-09-09 23:18:09 +0600

    feat: add custom user model

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,77 @@
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import PermissionsMixin
 from django.db import models
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("The email field must be set")
+
+        email = self.normalize_email(email)
+
+        user = self.model(
+            email=email,
+            **extra_fields
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True")
+
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True")
+
+        return self.create_user(
+            email=email,
+            password=password,
+            **extra_fields
+        )
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+
+    class Role(models.TextChoices):
+        CUSTOMER = "CUSTOMER", "Customer"
+        VENDOR = "VENDOR", "Vendor"
+        ADMIN = "ADMIN", "Admin"
+
+    email = models.EmailField(unique=True, db_index=True)
+
+    username = models.CharField(max_length=150,unique=True)
+
+    first_name = models.CharField(max_length=150,blank=True)
+
+    last_name = models.CharField(max_length=150,blank=True)
+
+    role = models.CharField(
+        max_length=20,
+        choices=Role.choices,
+        default=Role.CUSTOMER
+    )
+
+    is_active = models.BooleanField(default=True)
+
+    is_staff = models.BooleanField(default=False)
+
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
+
+    REQUIRED_FIELDS = ["username"]
+
+    def __str__(self):
+        return self.email
+
